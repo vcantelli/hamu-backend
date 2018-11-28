e7de39af6cb72096c1ebcb24b7de26c27b247ede
Victor Cantelli <victor.calegon@gmail.com>
2018-11-28 08:39:41 -0200

    nova tentativa

--- app.js
+++ app.js
@@ -6,12 +6,27 @@ var morgan = require('morgan')
 
 const http = require('http').Server(app)
 const port = process.env.PORT || 8080
+const url = require('url')
+const fixieUrl = url.parse('http://fixie:ZZksOn1Ml8Qcm8x@velodrome.usefixie.com:80')
+const requestUrl = url.parse('https://hamu.herokuapp.com')
 
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(morgan('dev')) // log every request to the console
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+app.get({
+  host: fixieUrl.hostname,
+  port: fixieUrl.port,
+  path: requestUrl.href,
+  headers: {
+    Host: requestUrl.host,
+    'Proxy-Authorization': `Basic ${new Buffer(fixieUrl.auth).toString('base64')}`,
+  }
+}, res => {
+console.log(`Got response: ${res.statusCode}`);
+});
 
 // view engine setup
 app.set('view engine', 'ejs')
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,25 +1,13 @@
 
 const vendorsController = require('../controllers').vendors
-const url = require('url')
-const fixieUrl = url.parse('http://fixie:ZZksOn1Ml8Qcm8x@velodrome.usefixie.com:80')
-const requestUrl = url.parse('/vendors/checkPassword')
 
 module.exports = (apiRoutes) => {
-  apiRoutes.get({
-      host: fixieUrl.hostname,
-      port: fixieUrl.port,
-      path: requestUrl.href,
-      headers: {
-        Host: requestUrl.host,
-        'Proxy-Authorization': `Basic ${new Buffer(fixieUrl.auth).toString('base64')}`,
-      }
-  }, vendorsController.checkPassword)
   apiRoutes.get('/', (req, res) => res.send('API v1'))
   apiRoutes.get('/vendors/list', vendorsController.list)
   apiRoutes.post('/vendors/create', vendorsController.create)
   apiRoutes.post('/vendors/edit', vendorsController.edit)
   apiRoutes.post('/vendors/destroy', vendorsController.destroy)
-  // apiRoutes.get('/vendors/checkPassword', vendorsController.checkPassword)
+  apiRoutes.get('/vendors/checkPassword', vendorsController.checkPassword)
   apiRoutes.get('/vendors/addImage', vendorsController.addImage)
   apiRoutes.post('/vendors/createProduct', vendorsController.createProduct)
   apiRoutes.get('/vendors/getProduct', vendorsController.getProduct)
