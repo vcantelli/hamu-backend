59a1eb8d6cb2293d54f78d1de5f1a54d0d676421
Victor Cantelli <victor.calegon@gmail.com>
2018-11-28 07:39:22 -0200

    Ajuste no fixie

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,21 @@
 
 const vendorsController = require('../controllers').vendors
+const url = require('url')
+const fixieUrl = url.parse('http://fixie:ZZksOn1Ml8Qcm8x@velodrome.usefixie.com:80')
+const requestUrl = url.parse('https://hamu.herokuapp.com')
 
 module.exports = (apiRoutes) => {
+  apiRoutes.get({
+      host: fixieUrl.hostname,
+      port: fixieUrl.port,
+      path: requestUrl.href,
+      headers: {
+        Host: requestUrl.host,
+        'Proxy-Authorization': `Basic ${new Buffer(fixieUrl.auth).toString('base64')}`,
+      }
+  }, res => {
+    console.log(`Got response: ${res.statusCode}`)
+  })
   apiRoutes.get('/', (req, res) => res.send('API v1'))
   apiRoutes.get('/vendors/list', vendorsController.list)
   apiRoutes.post('/vendors/create', vendorsController.create)
