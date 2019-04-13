eecaf43a047c6eabda23d3526d468144aa5e4165
Nelson Antunes <neryuuk@neryuuk.com>
2019-04-13 13:29:34 -0300

    Removendo comentario

--- test/test.js
+++ test/test.js
@@ -7,7 +7,6 @@ chai.use(chaiHttp)
 describe('Vendors', function () {
   this.timeout(5000)
   it('should receive a list of vendors on GET /vendors/list', done => {
-    // chai.request(app.server)
     chai.request(server).get(
       '/api/vendors/list'
     ).end((_erro, resposta) => {
