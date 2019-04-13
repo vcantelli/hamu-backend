10ee5d0b761d3a587a98987007e84e9763d57a2c
Nelson Antunes <neryuuk@neryuuk.com>
2019-04-13 13:04:02 -0300

    Utilizando teste existente com lista de vendors

--- test/test.js
+++ test/test.js
@@ -1,17 +1,18 @@
-var chai = require("chai")
+const chai = require("chai")
 const chaiHttp = require('chai-http')
-const should = chai.should()
 const server = require('../app')
+chai.should()
 chai.use(chaiHttp)
 
-describe('Inventions', function () {
-  it('should receive a list of inventions on GET /invention/list', done => {
+describe('Vendors', function () {
+  this.timeout(4000)
+  it('should receive a list of vendors on GET /vendors/list', done => {
     // chai.request(app.server)
-    chai.request(server)
-      .get('/api/invention/list')
-      .end((_erro, resposta) => {
-        resposta.should.have.status(200)
-        done()
-      })
+    chai.request(server).get(
+      '/api/vendors/list'
+    ).end((_erro, resposta) => {
+      resposta.should.have.status(200)
+      done()
+    })
   })
 })
