4c6343d1d1396f81e9fcd627e5fdb4c86d6bbf9a
Nelson Antunes <neryuuk@neryuuk.com>
2019-04-13 13:32:50 -0300

    ¯\_(ツ)_/¯

--- test/test.js
+++ test/test.js
@@ -7,9 +7,7 @@ chai.use(chaiHttp)
 describe('Vendors', function () {
   this.timeout(5000)
   it('should receive a list of vendors on GET /vendors/list', done => {
-    chai.request(server).get(
-      '/api/vendors/list'
-    ).end((_erro, resposta) => {
+    chai.request(server).get('/api/vendors/list').end((_erro, resposta) => {
       resposta.should.have.status(200)
       done()
     })
