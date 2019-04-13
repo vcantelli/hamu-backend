e0ced4c80163d9ba5f0e35d1698d31d7c912299d
Nelson Antunes <neryuuk@neryuuk.com>
2019-04-13 16:12:31 -0300

    Grouping endpoints

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,28 +1,11 @@
 const express = require('express')
-const {
-  list,
-  create,
-  edit,
-  destroy,
-  checkPassword,
-  addImage,
-  createProduct,
-  getProduct,
-  editProduct,
-  checkFacebookId
-} = require('../controllers').vendorsController
 const routes = express.Router()
+const vendors = require('./vendors')
 
-routes.get('/', (_requisicao, resposta) => { resposta.send('API v1') })
-routes.get('/vendors/list', list)
-routes.post('/vendors/create', create)
-routes.post('/vendors/edit', edit)
-routes.post('/vendors/destroy', destroy)
-routes.get('/vendors/checkPassword', checkPassword)
-routes.get('/vendors/addImage', addImage)
-routes.post('/vendors/createProduct', createProduct)
-routes.get('/vendors/getProduct', getProduct)
-routes.post('/vendors/editProduct', editProduct)
-routes.get('/vendors/checkFacebookId', checkFacebookId)
+routes.get('/', function (_requisicao, resposta) {
+  resposta.send('API v1')
+})
+
+routes.use('/vendors', vendors)
 
 module.exports = routes
---
+++ server/routes/vendors.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const routes = express.Router()
+const {
+  list,
+  create,
+  edit,
+  destroy,
+  checkPassword,
+  addImage,
+  createProduct,
+  getProduct,
+  editProduct,
+  checkFacebookId
+} = require('../controllers').vendorsController
+
+routes.get('/list', list)
+routes.post('/create', create)
+routes.post('/edit', edit)
+routes.post('/destroy', destroy)
+routes.get('/checkPassword', checkPassword)
+routes.get('/addImage', addImage)
+routes.post('/createProduct', createProduct)
+routes.get('/getProduct', getProduct)
+routes.post('/editProduct', editProduct)
+routes.get('/checkFacebookId', checkFacebookId)
+
+module.exports = routes
