64995661a68bed293397698ed03a2e610c83959c
Nelson Antunes <neryuuk@neryuuk.com>
2019-04-13 13:11:02 -0300

    Timeout de 5 segs

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ chai.should()
 chai.use(chaiHttp)
 
 describe('Vendors', function () {
-  this.timeout(4000)
+  this.timeout(5000)
   it('should receive a list of vendors on GET /vendors/list', done => {
     // chai.request(app.server)
     chai.request(server).get(
