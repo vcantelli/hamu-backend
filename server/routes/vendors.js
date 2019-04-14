72d9423aa423b9c999e880afe6a49fd78f981c61
Nelson Antunes <neryuuk@neryuuk.com>
2019-04-14 13:24:27 -0300

    Proposing new routes

--- server/routes/vendors.js
+++ server/routes/vendors.js
@@ -24,4 +24,16 @@ routes.get('/getProduct', getProduct)
 routes.post('/editProduct', editProduct)
 routes.get('/checkFacebookId', checkFacebookId)
 
+// Proposed new routes // Let's discuss it first
+// routes.post('/', create) // Create new vendor
+// routes.put('/:vendorId', edit) // Edit the vendor :vendorId
+// routes.post('/:vendorId/products', createProduct) // Create new product
+// routes.get('/:vendorId/products', list) // List all products from that vendor
+// routes.get('/:vendorId/products/:productId', getProduct) // Get a product from that vendor
+// routes.put('/:vendorId/products/:productId', editProduct) // Edit a product from that vendor
+// routes.post('/:vendorId/products/:productId/image', addImage) // Add new image to that product
+// routes.post('/destroy', destroy) // ???
+// routes.get('/password/check', checkPassword) // Check username and password
+// routes.get('/facebook-id/check', checkFacebookId) // Check FacebookID
+
 module.exports = routes
