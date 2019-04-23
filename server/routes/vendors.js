835a7146b12f06b9c8f213c626eace39b75548da
Nelson Antunes <neryuuk@neryuuk.com>
2019-04-23 07:18:42 -0300

    Finalizando rotas

--- server/routes/vendors.js
+++ server/routes/vendors.js
@@ -2,37 +2,25 @@ const express = require('express')
 const routes = express.Router()
 const { auth, vendors } = require('../controllers')
 
-// Proposed new routes // Let's discuss it first
-routes.route('/login').post(vendors.checkPassword)
+routes.route('/login')
+  .post(vendors.checkPassword)
 
 routes.route('/')
-  .post(vendors.create) // Create new vendor
-  .put(auth, vendors.edit) // Edit the vendor :vendorId
+  .post(vendors.create)
+  .put(auth, vendors.edit)
 
 routes.route('/products').all(auth)
-  .post(vendors.createProduct) // Create new product
-  .get(vendors.list) // List all products from that vendor
+  .post(vendors.createProduct)
+  .get(vendors.listProducts)
 
 routes.route('/products/:productId').all(auth)
-  .get(vendors.getProduct) // Get a product from that vendor
-  .put(vendors.editProduct) // Edit a product from that vendor
+  .get(vendors.getProduct)
+  .put(vendors.editProduct)
 
 routes.route('/products/:productId/image').all(auth)
-  .post(vendors.addImage) // Add new image to that product
+  .put(vendors.addImage)
 
 routes.route('/destroy').all(auth)
-  .post(vendors.destroy) // ???
-
-// Old routes
-// routes.get('/list', list)
-// routes.post('/create', create)
-// routes.post('/edit', edit)
-// routes.post('/destroy', destroy)
-// routes.get('/checkPassword', checkPassword)
-// routes.post('/addImage', addImage)
-// routes.post('/createProduct', createProduct)
-// routes.get('/getProduct', getProduct)
-// routes.post('/editProduct', editProduct)
-// routes.get('/checkFacebookId', checkFacebookId)
+  .post(vendors.destroy)
 
 module.exports = routes
