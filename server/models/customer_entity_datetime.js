ae99a3cea7a4117170753d989dfc1746b6cdcfcf
Victor Cantelli <v.cantelli@hotmail.com>
2019-05-02 05:06:44 -0300

    Criado entity de datetime para customers

---
+++ server/models/customer_entity_datetime.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = function (sequelize, DataTypes) {
+  var customerEntityDateTime = sequelize.define(
+    'customer_entity_datetime',
+    {
+      value_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+      },
+      entity_type_id: DataTypes.INTEGER,
+      attribute_id: DataTypes.INTEGER,
+      entity_id: DataTypes.INTEGER,
+      value: DataTypes.DATE
+    }, {
+      timestamps: false,
+      freezeTableName: true
+    }
+  )
+
+  return customerEntityDateTime
+}
