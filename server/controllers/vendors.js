8e43b3da3838ed4e36ea98a7012e4415a77456ed
Victor Cantelli <v.cantelli@hotmail.com>
2019-05-02 13:08:20 -0300

    Aplicado verificação do estoque na edição

--- server/controllers/vendors.js
+++ server/controllers/vendors.js
@@ -386,7 +386,18 @@ module.exports = {
       price: req.body.price,
       name: req.body.name,
       description: req.body.description,
-      short_description: req.body.shortDescription
+      short_description: req.body.shortDescription,
+      stock_data: {
+        qty: req.body.quantity,
+        is_in_stock: 1,
+        manage_stock: 1,
+        use_config_manage_stock: 1,
+        use_config_min_qty: 1,
+        use_config_min_sale_qty: 1,
+        use_config_max_sale_qty: 1,
+        use_config_backorders: 1,
+        use_config_notify_stock_qty: 1
+      }
     }
     magento.login(function (err, sessId) {
       if (err) return res.status(500).send(err)
