c19c2786f6ea88e83531078b4a8059b8a404bf76
Victor Cantelli <v.cantelli@hotmail.com>
2019-05-08 01:33:24 -0300

    Alterando rotas

--- server/routes/vendors.js
+++ server/routes/vendors.js
@@ -9,6 +9,9 @@ routes.route('/')
   .post(vendors.create)
   .put(auth, vendors.edit)
 
+routes.route('/bank/codes')
+.get(vendors.getBankCodes)
+
 routes.route('/token').all(auth)
   .post(vendors.registerToken)
 
@@ -16,7 +19,7 @@ routes.route('/products').all(auth)
   .post(vendors.createProduct)
   .get(vendors.listProducts)
 
-routes.route('/products/categories').all(auth)
+routes.route('/products/categories')
   .get(vendors.getCategoriesList)
 
 routes.route('/products/:productId').all(auth)
