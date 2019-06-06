1820bc0a80bdda48a9bfdd634df9548293af32cc
Nelson Antunes <neryuuk@neryuuk.com>
2019-06-06 08:52:04 -0300

    Typecast para Number

--- server/controllers/vendors.js
+++ server/controllers/vendors.js
@@ -583,7 +583,8 @@ function recoverMarketplaceVendor (customerId, email) {
   return new Promise(function (resolve, reject) {
     cedCsmarketplaceVendorInt.find({
       where: {
-        attribute_id: CUSTOMER_ID, value: customerId
+        attribute_id: CUSTOMER_ID,
+        value: Number(customerId)
       }
     }).then(({ entity_id }) => {
       return Promise.all([
