f09068ab0d3e94332f4c5450ad13c4ae888f25cb
Guilherme Oliveira <guilhermeborgesoliveira@gmail.com>
2019-08-04 20:13:40 -0300

    Criado helper de requisicao

---
+++ server/helpers/router.js
@@ -0,0 +1,33 @@
+module.exports = {
+  success,
+  fail
+}
+
+
+function success (httpResponse, options) {
+  return function (data) {
+    const response = { success: true, data }
+
+    if (options && options.csv) {
+      const fileExtension = options.fileExtension || 'csv'
+      httpResponse.writeHead(200, {
+        'Content-Length': Buffer.byteLength(data),
+        'Content-Type': 'text/csv; charset=utf-8;',
+        'Content-Disposition': `inline; filename="${options.name || `arquivo`}.${fileExtension}";`
+      })
+      httpResponse.end(data)
+    } else httpResponse.status(200).json(response)
+  }
+}
+
+function fail (httpResponse, status) {
+  return function (errors) {
+    const response = {
+      success: false,
+      message: errors.message,
+      errors: errors.errors
+    }
+    console.error((new Date()).toISOString(), errors)
+    httpResponse.status(status || 500).json(response)
+  }
+}
