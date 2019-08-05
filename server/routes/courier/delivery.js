6036a61990695b551cb4b1a9305d573dce15dedb
Guilherme Oliveira <guilhermeborgesoliveira@gmail.com>
2019-08-04 21:08:27 -0300

    Adicionando try catch para tratar todos os erros

--- server/routes/courier/delivery.js
+++ server/routes/courier/delivery.js
@@ -6,33 +6,41 @@ const { success, fail } = require('../../helpers/router')
 routes.route('/:orderNumber/route')
   .all(auth)
   .get(({params, decoded}, response) => {
-    courier.delivery.getRoute(params.orderNumber, decoded)
+    try {
+      courier.delivery.getRoute(params.orderNumber, decoded)
       .then(success(response))
       .catch(fail(response))
+    } catch(e) {fail(response)(e)}
   })
 
 routes.route('/:orderNumber/success')
   .all(auth)
   .post(({params, body, decoded}, response) => {
-    courier.delivery.success(params.orderNumber, body, decoded)
+    try {
+      courier.delivery.success(params.orderNumber, body, decoded)
       .then(success(response))
       .catch(fail(response))
+    } catch(e) {fail(response)(e)}
   })
 
 routes.route('/:orderNumber/problem')
   .all(auth)
   .post(({params, body, decoded}, response) => {
-    courier.delivery.problem(params.orderNumber, body, decoded)
+    try {
+      courier.delivery.problem(params.orderNumber, body, decoded)
       .then(success(response))
       .catch(fail(response))
+    } catch(e) {fail(response)(e)}
   })
 
 routes.route('/problem/reasons')
   .all(auth)
   .get((_, response) => {
-    courier.delivery.getProblemReasons()
+    try {
+      courier.delivery.getProblemReasons()
       .then(success(response))
       .catch(fail(response))
+    } catch(e) {fail(response)(e)}
   })
 
 module.exports = routes
