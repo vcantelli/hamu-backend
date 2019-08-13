4d486b694520fea7018217ba19ca353dada387ec
Guilherme Oliveira <guilhermeborgesoliveira@gmail.com>
2019-08-12 21:31:35 -0300

    sou burro
    Co-authored-by: Nelson Antunes <neryuuk@neryuuk.com>

--- server/routes/courier/index.js
+++ server/routes/courier/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const routes = express.Router()
-const registrarionRoutes = require('./registration')
+const registrationRoutes = require('./registration')
 const authRoutes = require('./auth')
 const pickUpRoutes = require('./pickUp')
 const deliveryRoutes = require('./delivery')
 const reportRoutes = require('./report')
 
 
-routes.use('/', registrarionRoutes)
+routes.use('/', registrationRoutes)
 routes.use('/', authRoutes)
 routes.use('/pick-up', pickUpRoutes)
 routes.use('/delivery', deliveryRoutes)
