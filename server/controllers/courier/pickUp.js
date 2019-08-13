79702d15735cfdf111b9d00a24498dca488a817d
Guilherme Oliveira <guilhermeborgesoliveira@gmail.com>
2019-08-12 22:22:53 -0300

    Mock do pickUp

--- server/controllers/courier/pickUp.js
+++ server/controllers/courier/pickUp.js
@@ -1,5 +1,20 @@
-function hasNewRequest () {
-  return Promise.resolve('hasNewRequest')
+function hasNewRequest (userId) {
+  return Promise.resolve(searchDatabaseForNewRequest(userId))
+    .then(filterData)
+
+  function searchDatabaseForNewRequest (userId) {
+    return Promise.resolve('FULL DATA')
+  }
+
+  function filterData (data) {
+    return Promise.resolve({
+      store: 'Arco Íris',
+      store_address: 'Av das Nações, 1916, Parque Oratório, Santo André - SP, 09270-400',
+      telephone: '1144011755',
+      order_number: '853291',
+      vehicle_type: 'motorcycle'
+    })
+  }
 }
 
 function getPickupInfo () {
