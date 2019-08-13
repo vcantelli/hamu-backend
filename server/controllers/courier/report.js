a1dce08ee75bd8df7a52637012613a8c8f182101
Guilherme Oliveira <guilhermeborgesoliveira@gmail.com>
2019-08-12 23:43:52 -0300

    Mockzin final

--- server/controllers/courier/report.js
+++ server/controllers/courier/report.js
@@ -1,5 +1,48 @@
 function orders () {
-  return Promise.resolve('orders')
+  return Promise.resolve({
+    'orders': [
+      {
+        'order_number': '84578',
+        'distance': 12000,
+        'delivery_price': 800
+      },
+      {
+        'order_number': '84578',
+        'distance': 12000,
+        'delivery_price': 800
+      },
+      {
+        'order_number': '84578',
+        'distance': 12000,
+        'delivery_price': 800
+      },
+      {
+        'order_number': '84578',
+        'distance': 12000,
+        'delivery_price': 800
+      },
+      {
+        'order_number': '84578',
+        'distance': 12000,
+        'delivery_price': 800
+      },
+      {
+        'order_number': '84578',
+        'distance': 12000,
+        'delivery_price': 800
+      },
+      {
+        'order_number': '84578',
+        'distance': 12000,
+        'delivery_price': 800
+      },
+      {
+        'order_number': '84578',
+        'distance': 12000,
+        'delivery_price': 800
+      }
+    ]
+  })
 }
 
 module.exports = {
