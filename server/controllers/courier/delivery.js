0997c4f9e4ac888fc8c55991ffef41bbf9906019
Guilherme Oliveira <guilhermeborgesoliveira@gmail.com>
2019-08-12 23:35:28 -0300

    Mock para buscar a rota

--- server/controllers/courier/delivery.js
+++ server/controllers/courier/delivery.js
@@ -5,8 +5,29 @@ module.exports = {
   getProblemReasons
 }
 
-function getRoute () {
-  return Promise.resolve('getRoute')
+function getRoute (orderNumber, userId) {
+  return getDeliveryInfo(orderNumber, userId)
+    .then(filterData)
+
+  function getDeliveryInfo (orderNumber, userId) {
+    const orderIsNotOnDelivering = false
+    const userIsNotAllowed = false
+    if (orderIsNotOnDelivering)
+      throw { code: `ORDER_STATUS_INVALID`, message: `Pedido não está em estatus de entrega`, status: 400 }
+    if (userIsNotAllowed)
+      throw { code: `NOT_ALLOWED`, message: `Você não pode ter acesso a este pedido`, status: 403 }
+    return Promise.resolve({ orderNumber })
+  }
+
+  function filterData (data) {
+    return Promise.resolve({
+      delivery_price: 800,
+      customer: "Estevão Trabalhos",
+      address: "Waverley Street, 2101, Old Palo Alto, Palo Alto - CA 94301",
+      telephone: "11999999999",
+      order_number: `${data.orderNumber}`
+    })
+  }
 }
 
 function success () {
