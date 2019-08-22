adc4b268b210ec1e3fcfe3095823f4a2d74842a4
Guilherme Oliveira <guilhermeborgesoliveira@gmail.com>
2019-08-22 01:32:11 -0300

    implementando funções de update

--- server/models/courier/Courier.js
+++ server/models/courier/Courier.js
@@ -6,9 +6,11 @@ const { promisify } = require('util')
 const Magento = new MagentoAPI(magentoConfig)
 Magento.login = promisify(Magento.login).bind(Magento)
 Magento.customer.create = promisify(Magento.customer.create).bind(Magento.customer)
+Magento.customer.update = promisify(Magento.customer.update).bind(Magento.customer)
 
 module.exports = {
   create,
+  update,
   getById,
   getByMagentoId
 }
@@ -31,16 +33,56 @@ function create (data) {
     data.images_user = '' // data.images_user || ''
     data.images_vehicle = '' // data.images_vehicle || ''
     data.images_driver_license = '' // data.images_driver_license || ''
+    data = convertDatesToDatabase(data)
     return CourierDAO.create(data)
+  })
+}
+
+function update (customer_id, data) {
+  return Magento.login().then(() => {
+    const customerData = {
+      email: data.email,
+      firstname: data.name.split(' ')[0],
+      lastname: data.name.substr(data.name.indexOf(' ')).trim(),
+      website_id: 1,
+      store_id: 1,
+      group_id: 1
+    }
+    if (data.password) customerData.password = data.password
+
+    return Magento.customer.update({
+      customerId: customer_id,
+      customerData
+    })
+    .then(() => {
+      data.images_user = '' // data.images_user || ''
+      data.images_vehicle = '' // data.images_vehicle || ''
+      data.images_driver_license = '' // data.images_driver_license || ''
+      data = convertDatesToDatabase(data)
+      return CourierDAO.update(data, {where: {customer_id}})
+    })
   })
 }
 
 function getById (courier_id) {
   return CourierDAO.find({ where: { courier_id, status: 'ACTIVE'}})
-    .then(data => data && data.dataValues)
+    .then(data => data && data.dataValues && convertDatesToSystem(data.dataValues) )
 }
 
 function getByMagentoId (customer_id) {
   return CourierDAO.find({ where: { customer_id, status: 'ACTIVE'}})
-    .then(data => data && data.dataValues)
+    .then(data => data && data.dataValues && convertDatesToSystem(data.dataValues) )
+}
+
+
+function convertDatesToDatabase (data) {
+  data.date_of_birth = data.date_of_birth
+  data.driver_license_expiry_date = data.driver_license_expiry_date
+  return data
+}
+
+function convertDatesToSystem (data) {
+  data.date_of_birth = data.date_of_birth
+  data.driver_license_expiry_date = data.driver_license_expiry_date
+  return data
 }
